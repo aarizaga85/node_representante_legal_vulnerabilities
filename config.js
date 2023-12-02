c13970e37e83c599df3970649b072e8c023a2e36
aarizaga85 <andresarizaga@gmail.com>
2023-12-01 21:49:21 -0500

    Correción vulnerabilidad A07
    Se corrige la vulnerabilidad de owasp a07

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+
+const user = process.env.MONGO_USER;
+const password = process.env.MONGO_PASSWORD;
+
 const config = {
     PORT: 3000, 
     SECRET:'INEXCELSISDEO',
-    DB_URL: 'mongodb+srv://belenarizaga7:belenarizaga7@cluster0.vqyufum.mongodb.net/'
+    DB_URL: 'mongodb+srv://{user}:{password}@cluster0.vqyufum.mongodb.net/'
 }
 
 module.exports = config
